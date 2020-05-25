bea812d3b807345e97e5d36419099be45299fcc8
Didi Medina <hi@didimedina.com>
2020-05-25 23:04:48 +0300

    configured chakra and framer motion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { ThemeProvider } from '@chakra-ui/core';
+import { motion } from "framer-motion";
+import { Box } from "@chakra-ui/core";
+import theme, { corner, spacer, text } from './theme';
+
+
+
 
 function App() {
+  const [density, setDensity] = useState('standard')
+  const actualTheme = theme(density)
+  const handleClick = () => setDensity('low')
+
+ 
+  // const motionBox = motion.custom(Box)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={actualTheme}>
+        <Box 
+          as="button" 
+          fontSize={text.t2}
+          rounded={corner.smooth} 
+          py={spacer.normal}
+          px={spacer.normal}
+          bg="tomato" 
+          color="white" 
+          whileHover={{ scale: 1.3 }}
+          >
+          Button
+        </Box>
+        <Box />
+        {/* 
+            - Is Box simply working without an sx prop bec it recognizes all the css props in its definition?
+            - Why cant i use the as prop to write motion.button and spit out a motion component?
+         */}
+        <motion.button
+          onClick={handleClick}
+           whileHover={{ scale: 1.3 }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          change density
+        </motion.button>
+
+    </ThemeProvider>
   );
 }
 
