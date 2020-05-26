0bc5d162c787fc0a529372005a5ff7d8eb2a584d
Didi Medina <hi@didimedina.com>
2020-05-26 11:12:54 +0300

    added static theme colors and framer motion

--- src/App.js
+++ src/App.js
@@ -1,46 +1,85 @@
 import React, { useState } from 'react';
 import { ThemeProvider } from '@chakra-ui/core';
 import { motion } from "framer-motion";
-import { Box } from "@chakra-ui/core";
-import theme, { corner, spacer, text } from './theme';
+import { Box, Text } from "@chakra-ui/core";
+import theme, { corner, spacer, text, heading } from './theme';
 
 
-
+// TODOs
+// Make it control theme dark/light
+// Make colors dynamic
+// Make one button toggle density
 
 function App() {
   const [density, setDensity] = useState('standard')
   const actualTheme = theme(density)
-  const handleClick = () => setDensity('low')
+  const handleDensify = () => setDensity('low')
+  const handleComfy = () => setDensity('high')
 
  
-  // const motionBox = motion.custom(Box)
+  const MotionBox = motion.custom(Box)
+  const MotionText = motion.custom(Text)
 
   return (
-    <ThemeProvider theme={actualTheme}>
-        <Box 
-          as="button" 
-          fontSize={text.t2}
-          rounded={corner.smooth} 
-          py={spacer.normal}
-          px={spacer.normal}
-          bg="tomato" 
-          color="white" 
-          whileHover={{ scale: 1.3 }}
-          >
-          Button
-        </Box>
-        <Box />
-        {/* 
-            - Is Box simply working without an sx prop bec it recognizes all the css props in its definition?
-            - Why cant i use the as prop to write motion.button and spit out a motion component?
-         */}
-        <motion.button
-          onClick={handleClick}
-           whileHover={{ scale: 1.3 }}
+    <ThemeProvider theme={actualTheme}>    
+      <Box
+        mx='auto'
+        width='100%'
+        maxWidth='720px'
+        marginTop={spacer.tight}
+      >
+        <Box
+          backgroundColor = "#F1F1F4"
+          padding={spacer.loose}
+          mx= {spacer.tight}
+          borderRadius= {corner.smoother}
         >
-          change density
-        </motion.button>
-
+          <Text 
+            fontSize={heading.h3}
+            m={spacer.flush}
+            color="#343B48"
+          >
+            We're testing out some amazing frontend tooling.
+          </Text>
+          <Text
+            fontSize={text.t1}
+            color="#636370"
+          >
+            Chakra-UI is replacing Theme-UI, but thats not an issue because they both use all the same 
+            technologies under the hood. Then we also included Framer Motion to make things
+            <MotionText as="span" ml={spacer.tightest} display="inline-block" color={"tomato"} whileHover={{ color: "green", scale: 1.1 }}> animate and pop.</MotionText>
+          </Text>
+          <MotionBox 
+            as="button" 
+            fontSize={text.t2}
+            rounded={corner.smooth} 
+            py={spacer.tighter}
+            px={spacer.normal}
+            bg="tomato" 
+            color="white"
+            border="none"
+            onClick={handleDensify}
+            whileTap={{ scale: 0.95 }}
+            mr={spacer.tighter}
+            >
+            Compact
+          </MotionBox>
+          <MotionBox 
+            as="button" 
+            fontSize={text.t2}
+            rounded={corner.smooth} 
+            py={spacer.tighter}
+            px={spacer.normal}
+            bg="tomato" 
+            color="white"
+            border="none"
+            onClick={handleComfy}
+            whileTap={{ scale: 0.95 }}
+            >
+            Comfort
+          </MotionBox>
+        </Box>
+      </Box>
     </ThemeProvider>
   );
 }
