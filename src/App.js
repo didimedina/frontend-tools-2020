9a576915e62e4cc9aff85b4e2d3ef333a62a9ee6
Didi Medina <hi@didimedina.com>
2020-05-27 22:10:37 +0300

    made theme work and adjusted the colors to have equal contrast when flipped to dark them

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { motion } from "framer-motion";
-import { Box, Text, useColorMode } from "@chakra-ui/core";
+import { Box, Text, useColorMode, useTheme } from "@chakra-ui/core";
 import { corner, spacer, text, heading } from './theme';
 import { useDensity } from './ThemeProvider'
 
 
 // TODOs
-// Make it control theme dark/light
 // Make colors dynamic
-// Make one button toggle density
 
 function App() {
   const [density, setDensity] = useDensity()
   const handleDensity = () => setDensity(density === 'compact' ? 'comfy' : 'compact')
   const { colorMode, toggleColorMode } = useColorMode('light');
   console.log(colorMode)
+  const theme = useTheme()
+  const color = theme.colors
+  const currentColor = color.gray
 
   const MotionBox = motion.custom(Box)
   const MotionText = motion.custom(Text)
+
 
 
   return (   
@@ -28,7 +30,7 @@ function App() {
       marginTop={spacer.tight}
     >
       <Box
-        backgroundColor = 'purple.50'
+        backgroundColor={currentColor[50]}
         padding={spacer.loose}
         mx= {spacer.tight}
         borderRadius= {corner.smoother}
@@ -36,17 +38,25 @@ function App() {
         <Text 
           fontSize={heading.h3}
           m={spacer.flush}
-          color='purple.900' // why is this not in curly braces? Would be nice to get auto complete...
+          color={currentColor[900]}
         >
           We're testing out some amazing frontend tooling.
         </Text>
         <Text
           fontSize={text.t1}
-          color='purple.700'
+          color={currentColor[600]}
         >
           Chakra-UI is replacing Theme-UI, but thats not an issue because they both use all the same 
           technologies under the hood. Then we also included Framer Motion to make things
-          <MotionText as="span" ml={spacer.tightest} display="inline-block" color='red.500' whileHover={{ color: 'red.200', scale: 1.1 }}> animate and pop.</MotionText>
+          <MotionText
+            as="span" 
+            ml={spacer.tightest} 
+            display="inline-block" 
+            color={color.red[500]} 
+            whileHover={{ color: currentColor['500'], scale: 1.1 }}
+            > 
+            animate and pop.
+            </MotionText>
         </Text>
         <MotionBox 
           as="button" 
@@ -54,8 +64,8 @@ function App() {
           rounded={corner.smooth} 
           py={spacer.tighter}
           px={spacer.normal}
-          bg='red.500' 
-          color="red.50"
+          bg={currentColor[300]} 
+          color={currentColor[800]}
           border="none"
           onClick={handleDensity}
           whileTap={{ scale: 0.95 }}
@@ -69,8 +79,8 @@ function App() {
           rounded={corner.smooth} 
           py={spacer.tighter}
           px={spacer.normal}
-          bg='purple.500'
-          color='purple.50'
+          bg={color.purple[500]}
+          color={currentColor[50]}
           border="none"
           onClick={toggleColorMode}
           whileTap={{ scale: 0.95 }}
