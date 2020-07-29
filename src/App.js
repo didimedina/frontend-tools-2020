c60b97bc4fde1bd768af42ff47c38a3d6d08cfd6
Didi Medina <hi@didimedina.com>
2020-07-29 14:23:56 +0300

    added PageModal component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import Sidebar from './components/Sidebar';
 import Inspector from './components/Inspector';
 import Body from './components/Body';
 import Modal from './components/Modal';
+import PageModal from './components/PageModal';
 
 
 const App = () => {
 
   return (
     <Box bg={color.grey[200]} w='100vw' h='100vh' position='relative'>
-      <Modal/>
+      {/* <Modal/> */}
+      <PageModal/>
       <Grid 
         w='100%'
         h='100%'
--- src/components/PageModal.js
+++ src/components/PageModal.js
@@ -2,7 +2,7 @@
 import { Box, Grid, Flex, jsx } from "@chakra-ui/core";
 import { color, space, corner, size, fontSize } from '../theme';
 
-const Modal = () => {
+const PageModal = () => {
     
     return (
         <Flex
@@ -11,31 +11,31 @@ const Modal = () => {
             bg={`${color.grey[700]}80`}
             position='absolute'
             justifyContent='center'
-            alignItems='center'
+            alignItems='flex-end'
             sx={{'backdrop-filter':'blur(4px)'}}
             >
             <Grid 
                 w='100%'
-                maxW={size.container.md}
-                minH='60%'
+                h='90%'
                 bg={color.base.white}
-                borderRadius={corner.smoother}
-                templateRows='min-content 1fr min-content'
-                templateAreas={`"header" "body" "footer"`}
+                templateRows='min-content 1fr'
+                templateColumns='100%'
                 p={space.normal}
                 gridGap={space.loose}
                 boxShadow={`0px 10px 32px ${color.grey[700]}20`}
+                // justifyContent='center'
             >
                 <Flex justifyContent='space-between' alignItems='center' bg={color.grey[100]} borderRadius={corner.smooth} >
                     <Box fontSize={fontSize.heading.lg} lineHeight='0px'>Hello World</Box>
                     <Box w={size.element.md} h={size.element.md} borderRadius={corner.pill} bg={color.grey[200]}></Box>
                 </Flex>
-                 
-                <Box w='100%' maxW={size.container.sm} bg={color.grey[100]} borderRadius={corner.smooth} justifySelf='center'></Box>  
-                <Box h='64px' bg={color.grey[100]} borderRadius={corner.smooth}></Box>             
+                <Grid templateRows='1fr min-content' gridGap={space.loose} maxW={size.container.lg} w='100%' justifySelf='center'>
+                    <Box w='100%' maxW={size.container.md} bg={color.grey[100]} borderRadius={corner.smooth} justifySelf='center'></Box>  
+                    <Box h='64px' bg={color.grey[100]} borderRadius={corner.smooth}></Box>  
+                </Grid>           
             </Grid>
         </Flex>
     );
 };
 
-export default Modal;
+export default PageModal;
